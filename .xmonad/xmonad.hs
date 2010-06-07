9396165c1a3090109fab8c8eb334e446babe8872
Tiger/Polar Bear <tiger.polarbear@gmail.com>
2010-06-06 23:40:33 -0700

    NoBorders action for xmonad.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,8 +2,9 @@ import XMonad hiding (Tall)
 import XMonad.Layout.HintedTile
 import XMonad.Layout.Magnifier
 import XMonad.Layout.HintedGrid
+import XMonad.Layout.NoBorders
 
-myLayout = HintedTile 2 (1/100) (2/3) TopLeft Tall ||| HintedTile 1 (1/100) (2/3) TopLeft Tall ||| Full
+myLayout = HintedTile 2 (1/100) (2/3) TopLeft Tall ||| HintedTile 1 (1/100) (2/3) TopLeft Tall ||| noBorders Full
 
 main = xmonad defaultConfig
 	{ modMask = mod4Mask -- Use Super instead of Alt
